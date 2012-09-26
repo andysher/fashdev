a4bbbdb3653e2b8b9f3e0b41cb803c136ce0effe
andysher <sherkar.aniruddha@gmail.com>
2012-09-26 15:51:26 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fashdev::Application.config.secret_token = '6d4d5e4e167e160332819d9af4499b147d0da65a4acbde83304b06fe1181037e3f289a0af8d4a19a55bb749216afa1db076f012f8b806a62b36a9eb9c878722e'
